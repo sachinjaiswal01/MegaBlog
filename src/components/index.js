eafe4a6aa430aca3e849735596ebd5e96ec7f153
sachin jaiswal <jayswalsachin012@gmail.com>
2024-11-24 14:44:55 +0530

    debugging

--- src/components/index.js
+++ src/components/index.js
@@ -3,13 +3,16 @@ import Footer from "./Footer/Footer";
 import Container from "postcss/lib/container";
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
-import RTE from "./RTE";
+import RTE from './RTE'
+import Button from "./Button" 
 import Signup from "./Signup"
 import Login from "../pages/Login";
-import Post from "../pages/Post";
+
 import PostForm from "./post-form/PostForm"
 import PostCard from "./PostCard"
 import AuthLayout from "./AuthLayout"
+import Input from "./Input";
+import Select from "./Select"
 
 export {
     Header,
@@ -18,10 +21,14 @@ export {
     Logo,
     LogoutBtn,
     RTE,
+    Button,
     Signup,
     Login,
-    Post,
+    
     PostForm,
     PostCard,
-    AuthLayout
+    AuthLayout,
+    Input,
+    Select,
+
 }
