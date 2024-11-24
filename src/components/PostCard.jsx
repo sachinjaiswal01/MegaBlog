b6d12558ddbbb02f91a834535a91a98665d3e5c9
sachin jaiswal <jayswalsachin012@gmail.com>
2024-11-24 14:18:18 +0530

    debugging PostCard

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -1,7 +1,7 @@
-import React from 'react'
-import appwriteService from " ../appwrite/config"
-import {Link} from 'react-router-dom
-'
+
+import appwriteService from "../appwrite/config"
+import {Link} from 'react-router-dom'
+
 function PostCard({$id, title, featureImage}){
 
   return (
